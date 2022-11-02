6df31fcbfce593f32026f3e44289947035bc0de7
suysoftware <suy.register@gmail.com>
2022-11-02 13:08:27 +0300

    delete

--- lib/src/blocs/explore_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class ExploreDataCubit extends Cubit<List<dynamic>> {
-  ExploreDataCubit() : super(["d"]);
-
-  void exploreChanger(newList) {
-    emit(newList);
-  }
-}
--- lib/src/model/group/group_chat_voice.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class GroupChatVoice {
-  late Icon icon;
-  late Duration duration;
-  late String itemUrl;
-
-  GroupChatVoice(this.icon, this.duration, this.itemUrl);
-}
